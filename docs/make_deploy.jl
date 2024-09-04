5bcb8111049dabf9051d6c15a8ccef8aeb0b2af6
Jannes Hühnerbein <j.huehnerbein@icloud.com>
2024-09-05 00:35:34 +0100

    updated Github action config

--- docs/make_deploy.jl
+++ docs/make_deploy.jl
@@ -1,5 +1,8 @@
 include("make.jl")
 
 println("Deploying Documentation...")
-deploydocs(repo = "github.com/janneshb/SimpleSim.jl.git")
+deploydocs(
+    devbranch = "docs",
+    repo = "github.com/janneshb/SimpleSim.jl.git"
+)
 println("Done!")
