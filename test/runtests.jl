e9ef302448e323a2480475728c7c925e9d9ffcf0
Jannes Huehnerbein <j.huehnerbein@icloud.com>
2024-09-05 19:31:16 +0100

    prepared better testing infrastructure

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,4 @@ using Test
 using SimpleSim
 
 include("test_integrators.jl")
+include("test_full_sims.jl")
---
+++ test/test_full_sims.jl
@@ -0,0 +1,16 @@
+@testset "Full Simulations" begin
+    @testset "Full Simulation CT" begin
+
+
+    end
+
+    @testset "Full Simulation DT" begin
+
+
+    end
+
+    @testset "Full Simulation Hybrid / Nested" begin
+
+
+    end
+end
