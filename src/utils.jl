7a84ccd5f2e15ae12065ef587b73893a12c8def0
Jannes Huehnerbein <j.huehnerbein@icloud.com>
2024-09-19 15:40:27 +0100

    debugged unitful rounding function

--- src/utils.jl
+++ src/utils.jl
@@ -13,6 +13,6 @@ gcd(x, y) = oneunit(x) * Base.gcd(x.val, y.val) # for Unitful.jl Quantities
 @inline round_unitful(x; kwargs...) = _round_unitful(x; kwargs...)
 @inline _round_unitful(x::Rational{T}; kwargs...) where {T<:Integer} =
     round(Int64, x; kwargs...)
-@inline _round_unitful(x::AbstractFloat; kwargs...) = round(Int64, x; kwargs...)
+@inline _round_unitful(x::AbstractFloat; kwargs...) = round(x; kwargs...)
 @inline _round_unitful(x::Integer; kwargs...) = x
 @inline _round_unitful(x; kwargs...) = round(typeof(oneunit(x)), x; kwargs...)
